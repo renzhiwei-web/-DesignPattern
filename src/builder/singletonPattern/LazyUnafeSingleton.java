2b77fde404fe53a6e197bb24abba70817510d190
renzhiwei-web <renzhiwei565@163.com>
2020-12-29 18:30:05 +0800

    单例设计模式

---
+++ src/builder/singletonPattern/LazyUnafeSingleton.java
@@ -0,0 +1,20 @@
+package builder.singletonPattern;
+
+/**
+ * 懒汉式单例模式，最大的问题就是线程不安全，所以严格意义上它并不算单例模式。
+ */
+public class LazyUnafeSingleton {
+    private static LazyUnafeSingleton instance;
+    private LazyUnafeSingleton(){}
+
+    public static LazyUnafeSingleton getInstance() {
+        if (instance == null){
+            instance = new LazyUnafeSingleton();
+        }
+        return instance;
+    }
+
+    public void showMessage(){
+        System.out.println("LazyUnafeSingleton");
+    }
+}
