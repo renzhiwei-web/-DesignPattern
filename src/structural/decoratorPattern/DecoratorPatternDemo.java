1df0fcb1704c406033655e88cd1bd4c8ba72454a
renzhiwei-web <renzhiwei565@163.com>
2020-12-30 14:32:04 +0800

    装饰设计模式的使用

--- src/structural/decoratorPattern/DecoratorPatternDemo.java
+++ src/structural/decoratorPattern/DecoratorPatternDemo.java
@@ -1,5 +1,9 @@
 package structural.decoratorPattern;
 
+/**
+ * 装饰器模式（Decorator Pattern）允许向一个现有的对象添加新的功能，同时又不改变其结构。
+ * 这种模式创建了一个装饰类，用来包装原有的类，并在保持类方法签名完整性的前提下，提供了额外的功能。
+ */
 public class DecoratorPatternDemo {
     public static void main(String[] args) {
 
@@ -16,5 +20,9 @@ public class DecoratorPatternDemo {
 
         System.out.println("\nRectangle of red border");
         redRectangle.draw();
+
+        ShapeDecorator blueShapeDecorator = new BlueShapeDecorator(new Circle());
+        System.out.println("\nCircle of blue border");
+        blueShapeDecorator.draw();
     }
 }
