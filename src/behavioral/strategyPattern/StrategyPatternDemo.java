f26793cc3f66d0ebfcb3dca6c310989a2807c909
renzhiwei-web <renzhiwei565@163.com>
2021-01-04 11:26:07 +0800

    策略设计模式

---
+++ src/behavioral/strategyPattern/StrategyPatternDemo.java
@@ -0,0 +1,19 @@
+package behavioral.strategyPattern;
+
+/**
+ * 在策略模式中，
+ * 我们创建表示各种策略的对象和一个行为随着策略对象改变而改变的 context 对象。
+ * 策略对象改变 context 对象的执行算法。
+ */
+public class StrategyPatternDemo {
+    public static void main(String[] args) {
+        Context context = new Context(new OperationAdd());
+        System.out.println("10 + 5 = " + context.executeStrategy(10,5));
+
+        context = new Context(new OperationSubtract());
+        System.out.println("10 - 5 = " + context.executeStrategy(10,5));
+
+        context = new Context(new OperationMultiply());
+        System.out.println("10 * 5 = " + context.executeStrategy(10,5));
+    }
+}
